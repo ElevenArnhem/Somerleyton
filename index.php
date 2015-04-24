c75854d6381a3bd08999a5af3e206c9b8dc4767f
thom peele <thom_peele@hotmail.com>
2015-04-24 12:29:19 +0200

    added basic php structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thom
+ * Date: 24-4-2015
+ * Time: 12:18
+ */
+include 'pageHead.php';
+
+echo '<title>Somerleyton Animal Park</title>';
+
+include 'container.php';
+
+include 'footer.php';
---
+++ pageHead.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thom
+ * Date: 24-4-2015
+ * Time: 12:19
+ */
+echo '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">';
+?>
