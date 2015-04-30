06606ec3e338f611be14e74a859f235d47e93e82
TonyNijenhuis <tonynijenhuis1993@gmail.com>
2015-04-30 11:59:41 +0200

    Tony commit

--- addSubSpecie.php
+++ addSubSpecie.php
@@ -3,22 +3,21 @@
 <form>
     <div class="form-group">
         <label>Naam</label>
-        <input type="text" class="form-control" placeholder="Text input">
+        <input type="text" class="form-control" placeholder="Naam dier">
     </div>
     <div class="form-group">
         <label>Latijnse naam</label>
-        <input type="text" class="form-control" placeholder="Text input">
+        <input type="text" class="form-control" placeholder="Latijnse naam">
     </div>
     <div class="form-group">
         <label>Hoofdsoort</label>
         <select class="form-control">
             <option>1</option>
-            <option>2</option>
-            <option>3</option>
         </select>
     </div>
     <div class="form-group">
         <label>Beschrijving</label>
         <textarea class="form-control" rows="3"></textarea>
     </div>
+    <button type="button" class="btn btn-default">Opslaan</button>
 </form>
