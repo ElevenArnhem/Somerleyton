bc426ada685d93ddac4186682d2faae9bcbbca17
Kayan Meijer <k.meijer1@live.nl>
2015-05-01 17:01:19 +0200

    Removed include

--- keeperHome.php
+++ keeperHome.php
@@ -5,5 +5,4 @@ echo '
           <h2>Dierverzorger Info</h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-       </div></div>';
-include 'findAnimal.php';
+       </div>';
