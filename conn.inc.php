3ab9b6e2b166ef3c5d9be9526da798686b3d5d07
thom peele <thom_peele@hotmail.com>
2015-05-03 13:45:47 +0200

    conn.inc changed

--- conn.inc.php
+++ conn.inc.php
@@ -2,10 +2,10 @@
 try {
     $hostname = "95.96.69.114";
     $dbname = "SomerleytonAnimalPark";
-<<<<<<< HEAD
-=======
+
+
     $port = "1433";
->>>>>>> ee5bb542702ed20020bee8d85edcc5f3fa804caa
+
     $username = "Eleven";
     $pw = "asdfasdf";
 
