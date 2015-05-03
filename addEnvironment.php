f07fa1785162e20d7fd8728fdbdd974e2bbf4934
thom peele <thom_peele@hotmail.com>
2015-05-04 00:35:21 +0200

    implemented add environment

--- addEnvironment.php
+++ addEnvironment.php
@@ -6,6 +6,16 @@
  * Time: 00:12
  */
 
+if(isset($_POST['ENVIRONMENTNAME'])) {
+    $staffID = $_SESSION['STAFFID'];
+    $environmentName = $_POST['ENVIRONMENTNAME'];
+    $addEnvironment = $dbh->prepare("proc_addEnvironment ?,?");
+    $addEnvironment->bindParam(1, $staffID);
+    $addEnvironment->bindParam(2, $environmentName);
+    $addEnvironment->execute();
+    spErrorCaching($addEnvironment);
+}
+
 echo '<div class="col-lg-4">
 
 <h1>Omgeving toevoegen</h1>
@@ -13,5 +23,6 @@ echo '<div class="col-lg-4">
  <dl class="dl-horizontal">
 <dt>Naam</dt><dd><input name="ENVIRONMENTNAME" type="text" class="form-control" value="" placeholder="gebied naam" required></dd><br>
 </dl>
+<button class="btn btn-primary" type="submit">Toevoegen</button>
 </form>
 </div>';
