30f7ce1c165f0a65bc54f5f94b3120d644339164
thom peele <thom_peele@hotmail.com>
2015-05-04 00:24:08 +0200

    started with add environment

--- addEnvironment.php
+++ addEnvironment.php
@@ -10,6 +10,8 @@ echo '<div class="col-lg-4">
 
 <h1>Omgeving toevoegen</h1>
 <form action="index.php?page=addEnvironment" method="post">
+ <dl class="dl-horizontal">
 <dt>Naam</dt><dd><input name="ENVIRONMENTNAME" type="text" class="form-control" value="" placeholder="gebied naam" required></dd><br>
+</dl>
 </form>
 </div>';
