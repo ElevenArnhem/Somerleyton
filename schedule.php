f2b2456b53b473c0675e76d37ceade8d1b2e9c5f
thom peele <thom_peele@hotmail.com>
2015-05-04 14:44:32 +0200

    started with schedule

---
+++ schedule.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thom
+ * Date: 4-5-2015
+ * Time: 14:41
+ */
+
+echo '
+<table class="table table-hover"><tr>
+            <th>Keeper</th>
+            <th>Begintijd</th>
+            <th>EindTijd</th>
+            <th>Waar</th>
+</tr>
+<tr>
+    <td>Thom</td>
+    <td>09:00</td>
+    <td>12:00</td>
+    <td>SeaLife zee vissen</td>
+    </tr>
+    </table>
+';
