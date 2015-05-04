e6c1f7d3c2f63792552fe9b7b38654800e0f1040
thom peele <thom_peele@hotmail.com>
2015-05-04 12:47:38 +0200

    implemented add area

--- addArea.php
+++ addArea.php
@@ -9,10 +9,12 @@ if(isset($_POST['ENVIRONMENTNAME'])) {
     $staffID = $_SESSION['STAFFID'];
     $environmentName = $_POST['ENVIRONMENTNAME'];
     $areaName = $_POST['AREANAME'];
+    $headKeeperID = $_POST['HEADKEEPER'];
     $addEnvironment = $dbh->prepare("proc_addArea ?,?,?,?");
     $addEnvironment->bindParam(1, $staffID);
     $addEnvironment->bindParam(2, $environmentName);
     $addEnvironment->bindParam(3, $areaName);
+    $addEnvironment->bindParam(4, $headKeeperID);
     $addEnvironment->execute();
     spErrorCaching($addEnvironment);
 }
@@ -41,9 +43,9 @@ foreach($environments as $environment) {
 echo'</select></dd><br>
 <dt>Hoofddierverzorger</dt><dd><select name="HEADKEEPER" type="text" class="form-control" >';
 foreach($allStaff as $staff) {
-    echo '<option value="'.$staff["StaffName"].'">'.$staff["StaffName"].'</option>';
+    echo '<option value="'.$staff["StaffID"].'">'.$staff["StaffName"].'</option>';
 }
-echo'
+echo'</select></dd><br>
 </dl>
 <button class="btn btn-primary" type="submit">Toevoegen</button>
 </form>
