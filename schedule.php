645004d1bf3ef0cc0b2d72cef23ed630c5267483
thom peele <thom_peele@hotmail.com>
2015-05-04 19:31:59 +0200

    started with schedule

--- schedule.php
+++ schedule.php
@@ -5,6 +5,13 @@
  * Date: 4-5-2015
  * Time: 14:41
  */
+if(isset($_GET['day'])) {
+
+}
+
+$keepersstmt = $dbh->prepare("EXEC proc_getKeepers");
+$keepersstmt->execute();
+$keepers = $keepersstmt->fetchAll();
 
 echo '
 <table class="table table-hover"><tr>
@@ -14,7 +21,11 @@ echo '
             <th>Waar</th>
 </tr>
 <tr>
-    <td>Thom</td>
+    <td><select name="KEEPER" type="text" class="form-control">';
+foreach($keepers as $keeper) {
+    echo '<option value="'.$keeper["StaffID"].'">'.$keeper["StaffName"].'</option>';
+}
+echo'</select></td>
     <td>09:00</td>
     <td>12:00</td>
     <td>SeaLife zee vissen</td>
