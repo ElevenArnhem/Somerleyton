a52d123dae707ccbf7bef1a4ddc1b3db05517b60
thom peele <thom_peele@hotmail.com>
2015-05-06 12:44:44 +0200

    started with schedule

--- schedule.php
+++ schedule.php
@@ -8,13 +8,22 @@
 if(isset($_GET['day'])) {
 
 }
+$staffID = $_SESSION['STAFFID'];
+
+$schedulestmt = $dbh->prepare("EXEC proc_getSchedule ?");
+$schedulestmt->bindParam(1, $staffID);
+$schedulestmt->execute();
+$schedule = $schedulestmt->fetchAll();
 
 $keepersstmt = $dbh->prepare("EXEC proc_getKeepers");
 $keepersstmt->execute();
 $keepers = $keepersstmt->fetchAll();
 
+$allEnvironments = $dbh->prepare("EXEC proc_GetEnvironment");
+$allEnvironments->execute();
+$environments = $allEnvironments->fetchAll();
+
 $allAreas = $dbh->prepare("EXEC proc_GetAreaName");
-//$allAreas->bindParam(1,$selectedEnivornment);
 $allAreas->execute();
 $areas = $allAreas->fetchAll();
 
@@ -23,8 +32,19 @@ echo '
             <th>Keeper</th>
             <th>Begintijd</th>
             <th>Eindtijd</th>
-            <th>Waar</th>
-</tr>
+            <th>Omgeving</th>
+            <th>Gebied</th>
+</tr>';
+foreach($schedule as $scheduleRow) {
+    echo '<tr>
+            <td>'.$scheduleRow["StaffID"].'</td>
+            <td>'.$scheduleRow["StartTime"].'</td>
+            <td>'.$scheduleRow["EndTime"].'</td>
+            <td>'.$scheduleRow["EnvironmentName"].'</td>
+            <td>'.$scheduleRow["AreaName"].'</td>
+</tr>';
+}
+echo '
 <tr><form action="index.php?page=schedule" method="post">
     <td><select name="KEEPER" type="text" class="form-control">';
 foreach($keepers as $keeper) {
@@ -33,6 +53,11 @@ foreach($keepers as $keeper) {
 echo'</select></td>
     <td><input name="STARTTIME" type="time" class="form-control" ></td>
     <td><input name="ENDTIME" type="time" class="form-control" ></td>
+    <td><select name="ENVIRONMENTNAME" type="text" class="form-control">';
+foreach($environments as $environment) {
+    echo '<option value="'.$environment["EnvironmentName"].'">'.$environment["EnvironmentName"].'</option>';
+}
+echo'</select></td>
     <td><select name="AREANAME" type="text" class="form-control">';
 foreach($areas as $area) {
     echo '<option value="'.$area["AreaName"].'">'.$area["AreaName"].'</option>';
