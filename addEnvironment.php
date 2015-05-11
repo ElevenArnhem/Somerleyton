773f1e0ec0fe489adc6c1242e3ba89a248551e54
thom peele <thom_peele@hotmail.com>
2015-05-11 13:38:30 +0200

    environment changed

--- addEnvironment.php
+++ addEnvironment.php
@@ -44,7 +44,7 @@ if(isset($_POST['TYPE'])) {
             $changeEnvironment->bindParam(2, $oldEnvironmentName);
             $changeEnvironment->execute();
             spErrorCaching($changeEnvironment);
-            header('index.php?page=environment');
+            http_redirect('index.php?page=environment');
         }
     }
 }
