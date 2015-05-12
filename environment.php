7975937a79b4544004588d2327d0b6815968c673
thom peele <thom_peele@hotmail.com>
2015-05-12 09:58:27 +0200

    addanimal changed

--- environment.php
+++ environment.php
@@ -86,7 +86,7 @@ echo "</table>";if($_SESSION['FUNCTION'] == 'KantoorPersoneel') {echo"<a href='i
     </tr>";
 foreach($enclosures as $enclosure) {
     echo '<tr>
-           <td>'.$enclosure["EnclosureID"].'</td> ';if($_SESSION['FUNCTION'] == 'HeadKeeper') {echo'<td>
+           <td>'.$enclosure["EnclosureID"].'</td> ';if($_SESSION['FUNCTION'] == 'HeadKeeper'|| $_SESSION['FUNCTION'] == 'KantoorPersoneel') {echo'<td>
 <form action="index.php?page=environment" method="post">
 <input type="hidden" name="ENCLOSURE" value="'.$enclosure["EnclosureID"].'">
 <input type="hidden" name="ENVIRONMENT" value="'.$selectedEnivornment.'">
