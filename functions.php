6105b58acb6511c25dfe1248cc801b6019c2421d
thom peele <thom_peele@hotmail.com>
2015-05-13 09:19:42 +0200

    change things, index does work local right know

--- functions.php
+++ functions.php
@@ -67,3 +67,14 @@ function addPicture($animalID) {
     }
 
 }
+function isLocal()
+{
+    $whitelist = array(
+        '127.0.0.1',
+        '::1'
+    );
+    if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+        return '../Somerleyton';
+    }
+    return '';
+}
