a8d05d4fc4d3ca24231983fe3961b71fb81ba3d0
Kayan Meijer <k.meijer1@live.nl>
2015-06-03 13:28:18 +0200

    Updated navbar and local order

--- navbar.php
+++ navbar.php
@@ -1,62 +1,71 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: thom
- * Date: 29-4-2015
- * Time: 12:25
- */
-echo '    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="navbar-fixed-top.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="navbar-fixed-top.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Dont actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<!-- Just for debugging purposes. Dont actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="js/ie-emulation-modes-warning.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <title>Someleyton animal park</title>
-  </head>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<title>Someleyton animal park</title>
+</head>
 
-  <body>
-
-      <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Somerleyton Animal Park</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="?page=medewerkers">Medewerkers</a></li>
-            <li><a href="?page=findAnimal">Dieren</a></li>
-            <li><a href="?page=species">Diersoorten</a></li>
-            <li><a href="?page=environment">Verblijven</a></li>
-            <li><a href="?page=feeding">Voeding</a></li>
-            <li><a href="?page=orders">Orders</a></li>
-            <li><a href="?page=Leverancier">Leveranciers</a></li>
-            <li><a href="?page=items">Producten</a></li>
-            <!--<li><a href="?page=addLocalOrder">Bestellijsten</a></li>-->
+<body>
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Somerleyton Animal Park</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li>
+            <a data-toggle="dropdown" role="button" aria-expanded="false">Dieren<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="?page=findAnimal">Dieren overzicht</a></li>
+                <li><a href="?page=species">Diersoorten</a></li>
+                <li><a href="?page=environment">Verblijven</a></li>
             </ul>
-          <ul class="nav navbar-nav navbar-right">
+        </li>
+        <li>
+              <a data-toggle="dropdown" role="button" aria-expanded="false">Orders<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                  <?php if($_SESSION['FUNCTION'] == 'KantoorPersoneel') { ?><li><a href="?page=orders">Order overzicht</a></li> <?php } ?>
+                  <?php if($_SESSION['FUNCTION'] == 'KantoorPersoneel') { ?><li><a href="?page=readLocalOrder">Bestellijsten</a></li> <?php } ?>
+                  <?php if($_SESSION['FUNCTION'] == 'HeadKeeper') { ?>
+                      <li>
+                          <form action="?page=addLocalOrder" method="post">
+                              <button type="submit" name="btnBestellijstBekijken" class="btn btn-link">Bestellijst bekijken</button>
+                          </form>
+                      </li>
+                  <?php } ?>
+              </ul>
+          </li>
+        <li><a href="?page=feeding">Voeding</a></li>
+        <?php if($_SESSION['FUNCTION'] == 'KantoorPersoneel') { ?> <li><a href="?page=medewerkers">Medewerkers</a></li> <?php } ?>
+        <?php if($_SESSION['FUNCTION'] == 'KantoorPersoneel') { ?><li><a href="?page=Leverancier">Leveranciers</a></li> <?php } ?>
+        <?php if($_SESSION['FUNCTION'] == 'KantoorPersoneel') { ?><li><a href="?page=items">Producten</a></li><?php } ?>
+        </ul>
+      <ul class="nav navbar-nav navbar-right">
 
-            <li><a href="logOut.php" >Log uit</a> </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>';
+        <li><a href="logOut.php" >Log uit</a> </li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
