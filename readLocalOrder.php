cbcf460986e2bd431edd54dccb7ca2d249e71d38
Kayan Meijer <k.meijer1@live.nl>
2015-06-03 16:05:34 +0200

    Default selected week set to this week number.

--- readLocalOrder.php
+++ readLocalOrder.php
@@ -1,31 +1,35 @@
 <?php
 if($_SESSION['FUNCTION'] == 'KantoorPersoneel') {
+    $staffID = $_SESSION['STAFFID'];
+    $currentWeek = getCurrentWeekNumber();
+    $currentYear = getCurrentYear();
+    $selectedWeek = $currentYear . '-W'. $currentWeek;
+
+    if (isset($_POST["Year_Week"])) {
+        $selectedWeek = $_POST['Year_Week'];
+        $currentWeek = explode("-W", $_POST["Year_Week"])[1];
+        $currentYear = explode("-W", $_POST["Year_Week"])[0];
+    }
+
+    echo '<h3>Week: ' . $currentWeek . '</h3>';
+    echo '<h3>Jaar: ' . $currentYear . '</h3>';
+
+    $readLocalOrderStatement = $dbh->prepare("EXEC proc_getLocalOrders ?, ?, ?");
+    $readLocalOrderStatement->bindParam(1, $staffID);
+    $readLocalOrderStatement->bindParam(2, $currentYear);
+    $readLocalOrderStatement->bindParam(3, $currentWeek);
+    $readLocalOrderStatement->execute();
+    $readLocalOrders = $readLocalOrderStatement->fetchAll();
+
     echo '
     <form action="index.php?page=readLocalOrder" method="POST">
       <div class="col-lg-4">
-        <br><br><input name="Year_Week" type="week" class="form-control" required>
+        <br><br><input name="Year_Week" type="week" value="'.$selectedWeek.'" class="form-control" required>
         <br><br><button class="btn btn-primary" type="submit">Weergeven</button><br><br>
       </div>
     </form>
-    ';
 
-    if (isset($_POST["Year_Week"])) {
-        $StaffID = $_SESSION["STAFFID"];
-        $week = explode("-W", $_POST["Year_Week"])[1];
-        $jaar = explode("-W", $_POST["Year_Week"])[0];
-
-        echo '<h3>Week: '.$week.'</h3>';
-        echo '<h3>Jaar: '.$jaar.'</h3>';
-
-        $readLocalOrderStatement = $dbh->prepare("EXEC proc_getLocalOrders ?, ?,?");
-        $readLocalOrderStatement->bindParam(1, $StaffID);
-        $readLocalOrderStatement->bindParam(2, $jaar);
-        $readLocalOrderStatement->bindParam(3, $week);
-        $readLocalOrderStatement->execute();
-        $readLocalOrders = $readLocalOrderStatement->fetchAll();
-
-        echo '
-        <table class="table table-hover" >
+    <table class="table table-hover" >
         <tr>
             <th>ProductID</th>
             <th>Naam</th>
@@ -41,6 +45,5 @@ if($_SESSION['FUNCTION'] == 'KantoorPersoneel') {
                 <td>' . $LocalOrder["Unit"] . '</td>
             </tr>';
         }
-        echo '</table>';
-    }
+echo '</table>';
 }
