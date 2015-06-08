71557cc32c6a80170b943111a8047c5b3b6d97b3
thom peele <thom_peele@hotmail.com>
2015-06-08 16:21:20 +0200

    changed error messages

--- addEnvironment.php
+++ addEnvironment.php
@@ -1,72 +1,86 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thom
- * Date: 4-5-2015
- * Time: 00:12
- */
-$pageTitle = 'Omgeving toevoegen';
-$environment = null;
-if(isset($_POST['ENVIRONMENT'])) {
-    $environment = $_POST['ENVIRONMENT'];
-    $pageTitle = 'Omgeving aanpassen';
-}
-if(isset($_POST['TYPE'])) {
-    if($_POST['TYPE'] == 'addEnvironment'){
-        if(isset($_POST['ENVIRONMENTNAME'])) {
-            $staffID = $_SESSION['STAFFID'];
-            $environmentName = $_POST['ENVIRONMENTNAME'];
-            $addEnvironment = $dbh->prepare("proc_addEnvironment ?,?");
-            $addEnvironment->bindParam(1, $staffID);
-            $addEnvironment->bindParam(2, $environmentName);
-            $addEnvironment->execute();
-            spErrorCaching($addEnvironment);
-            $environment = $environmentName;
+    /**
+     * Created by PhpStorm.
+     * User: thom
+     * Date: 4-5-2015
+     * Time: 00:12
+     */
+    if(canRead() && canCreate() && canUpdate()) {
+        $pageTitle = 'Omgeving toevoegen';
+        $environment = null;
+        if (isset($_POST['ENVIRONMENT'])) {
+            $environment = $_POST['ENVIRONMENT'];
+            $pageTitle = 'Omgeving aanpassen';
         }
-    } elseif($_POST['TYPE'] == 'changeEnvironment'){
-        if(isset($_POST['ENVIRONMENTNAME'])) {
-            $staffID = $_SESSION['STAFFID'];
-            $environmentName = $_POST['ENVIRONMENTNAME'];
-            $oldEnvironmentName = $_POST['OLDENVIRONMENTNAME'];
-            $changeEnvironment = $dbh->prepare("proc_alterEnvironment ?,?,?");
-            $changeEnvironment->bindParam(1, $staffID);
-            $changeEnvironment->bindParam(2, $oldEnvironmentName);
-            $changeEnvironment->bindParam(3, $environmentName);
-            $changeEnvironment->execute();
-            spErrorCaching($changeEnvironment);
-            $environment = $environmentName;
+        if (isset($_POST['TYPE'])) {
+            if ($_POST['TYPE'] == 'addEnvironment') {
+                if (isset($_POST['ENVIRONMENTNAME'])) {
+                    $staffID = $_SESSION['STAFFID'];
+                    $environmentName = $_POST['ENVIRONMENTNAME'];
+                    $addEnvironment = $dbh->prepare("proc_addEnvironment ?,?");
+                    $addEnvironment->bindParam(1, $staffID);
+                    $addEnvironment->bindParam(2, $environmentName);
+                    $addEnvironment->execute();
+                    spErrorCaching($addEnvironment);
+                    $environment = $environmentName;
+                }
+            } elseif ($_POST['TYPE'] == 'changeEnvironment') {
+                if (isset($_POST['ENVIRONMENTNAME'])) {
+                    $staffID = $_SESSION['STAFFID'];
+                    $environmentName = $_POST['ENVIRONMENTNAME'];
+                    $oldEnvironmentName = $_POST['OLDENVIRONMENTNAME'];
+                    $changeEnvironment = $dbh->prepare("proc_alterEnvironment ?,?,?");
+                    $changeEnvironment->bindParam(1, $staffID);
+                    $changeEnvironment->bindParam(2, $oldEnvironmentName);
+                    $changeEnvironment->bindParam(3, $environmentName);
+                    $changeEnvironment->execute();
+                    spErrorCaching($changeEnvironment);
+                    $environment = $environmentName;
+                }
+            } elseif ($_POST['TYPE'] == 'deleteEnvironment') {
+                if (isset($_POST['OLDENVIRONMENTNAME'])) {
+                    $staffID = $_SESSION['STAFFID'];
+                    $environmentName = $_POST['ENVIRONMENTNAME'];
+                    $oldEnvironmentName = $_POST['OLDENVIRONMENTNAME'];
+                    $changeEnvironment = $dbh->prepare("proc_deleteEnvironment ?,?");
+                    $changeEnvironment->bindParam(1, $staffID);
+                    $changeEnvironment->bindParam(2, $oldEnvironmentName);
+                    $changeEnvironment->execute();
+                    spErrorCaching($changeEnvironment);
+                }
+            }
         }
-    } elseif($_POST['TYPE'] == 'deleteEnvironment'){
-        if(isset($_POST['OLDENVIRONMENTNAME'])) {
-            $staffID = $_SESSION['STAFFID'];
-            $environmentName = $_POST['ENVIRONMENTNAME'];
-            $oldEnvironmentName = $_POST['OLDENVIRONMENTNAME'];
-            $changeEnvironment = $dbh->prepare("proc_deleteEnvironment ?,?");
-            $changeEnvironment->bindParam(1, $staffID);
-            $changeEnvironment->bindParam(2, $oldEnvironmentName);
-            $changeEnvironment->execute();
-            spErrorCaching($changeEnvironment);
-        }
+        echo '
+        <div class="col-lg-4">
+            <h1>'. $pageTitle . '</h1>
+            <form action="index.php?page=addEnvironment" method="post">
+                <dl class="dl-horizontal">
+                    <dt>Naam</dt>
+                    <dd>
+                        <input name="ENVIRONMENTNAME" type="text" class="form-control" value="';
+                            if (isset($_POST['ENVIRONMENT']) || isset($_POST['ENVIRONMENTNAME'])) {
+                                echo $environment;
+                            }
+                        echo '" placeholder="omgeving naam" required>
+                    </dd><br>
+                </dl>
+                <input name="OLDENVIRONMENTNAME" type="hidden"  value="';
+                    if (isset($_POST['ENVIRONMENT']) || isset($_POST['ENVIRONMENTNAME'])) {
+                        echo $environment;
+                    }
+                    echo '" >
+                <button class="btn btn-primary" name="TYPE" value="';
+                    if (isset($_POST['ENVIRONMENT'])) {
+                        echo 'changeEnvironment';
+                    } else {
+                        echo 'addEnvironment';
+                    }
+                    echo '" type="submit">Opslaan
+                </button>';
+                if (isset($_POST['ENVIRONMENT']) || isset($_POST['ENVIRONMENTNAME'])) {
+                    echo '<button class="btn btn-primary" name="TYPE" value="deleteEnvironment" type="submit">Verwijder omgeving</button>';
+                }
+                echo '
+            </form>
+        </div>';
     }
-}
-if(isset($_POST['CHANGEENVIRONMENTNAME'])) {
-//    @staffID INT,
-//	@originalName VARCHAR(50),
-//	@environmentName VARCHAR(50)
-}
-if(isset($_POST['DELETEENVIRONMENTNAME'])) {}
-
-
-
-echo '<div class="col-lg-4">
-
-<h1>'; echo $pageTitle.'</h1>
-<form action="index.php?page=addEnvironment" method="post">
- <dl class="dl-horizontal">
-<dt>Naam</dt><dd><input name="ENVIRONMENTNAME" type="text" class="form-control" value="'; if(isset($_POST['ENVIRONMENT']) || isset($_POST['ENVIRONMENTNAME'])) {echo $environment;} echo'" placeholder="omgeving naam" required></dd><br>
-</dl>
-<input name="OLDENVIRONMENTNAME" type="hidden"  value="'; if(isset($_POST['ENVIRONMENT']) || isset($_POST['ENVIRONMENTNAME'])) {echo $environment;} echo'" >
-<button class="btn btn-primary" name="TYPE" value="'; if(isset($_POST['ENVIRONMENT'])) {echo 'changeEnvironment';} else{echo 'addEnvironment';} echo'" type="submit">Opslaan</button>
-<button class="btn btn-primary" name="TYPE" value="deleteEnvironment" type="submit">Verwijder omgeving</button>
-</form>
-</div>';
--- addExchangeHistory.php
+++ addExchangeHistory.php
@@ -1,58 +1,59 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thom
- * Date: 4-6-2015
- * Time: 10:53
- */
+
+    /**
+     * Created by PhpStorm.
+     * User: thom
+     * Date: 4-6-2015
+     * Time: 10:53
+     */
 
 
-$animalID = $_GET['animalid'];
-$animalstmt = $dbh->prepare("proc_getAnimal ?");
-$animalstmt->bindParam(1, $animalID);
-$animalstmt->execute();
-$animal = $animalstmt->fetch();
+    $animalID = $_GET['animalid'];
+    $animalstmt = $dbh->prepare("proc_getAnimal ?");
+    $animalstmt->bindParam(1, $animalID);
+    $animalstmt->execute();
+    $animal = $animalstmt->fetch();
 
-$StaffID = $_SESSION["STAFFID"];
-
-
-$getAllZoosStatement = $dbh->prepare("EXEC proc_getAllZoos");
-$getAllZoosStatement->execute();
-$getAllZoos = $getAllZoosStatement->fetchall();
+    $StaffID = $_SESSION["STAFFID"];
 
 
-
-
-echo '<form action="?page=changeAnimal&animalID='.$animalID.'" method="post">
-      <div class="col-lg-8">
-          <h2>Uitwisseling toevoegen<h2>
-          <h2>'.$animal['AnimalName'].'</h2>
-          <br><br>
-          <p>Start datum</p>
-          <input name="senddate" type="Date" class="form-control" required>
-          <br><br>
-          <p>Dierentuin</p>';
-
-echo'<select name="ZooName" type="text" class="form-control" required>';
-
-foreach($getAllZoos as $zoo) {
-    echo ' <option>';  echo $zoo["ZooName"]; echo' </option>';
-}
-echo '</select>
-          <br><br>
-          <p>Uitwisseling type</p>
-          <select name="ExchangeType" type="text" class="form-control" required>
-                <option value="from" >Van</option>
-                <option value="to">Naar</option>
-          </select>
-          <br><br>
-          <p>Commentaar</p>
-          <textarea name="Comment" class="form-control" rows="5" maxlength="250" placeholder="Notities"></textarea>
-          <br><br>
+    $getAllZoosStatement = $dbh->prepare("EXEC proc_getAllZoos");
+    $getAllZoosStatement->execute();
+    $getAllZoos = $getAllZoosStatement->fetchall();
 
 
 
-          <button type="submit" class="btn btn-default">Opslaan</button>
-        </div>
-      </form>
-';
+
+    echo '<form action="?page=changeAnimal&animalID='.$animalID.'" method="post">
+          <div class="col-lg-8">
+              <h2>Uitwisseling toevoegen<h2>
+              <h2>'.$animal['AnimalName'].'</h2>
+              <br><br>
+              <p>Start datum</p>
+              <input name="senddate" type="Date" class="form-control" required>
+              <br><br>
+              <p>Dierentuin</p>';
+
+    echo'<select name="ZooName" type="text" class="form-control" required>';
+
+    foreach($getAllZoos as $zoo) {
+        echo ' <option>';  echo $zoo["ZooName"]; echo' </option>';
+    }
+    echo '</select>
+              <br><br>
+              <p>Uitwisseling type</p>
+              <select name="ExchangeType" type="text" class="form-control" required>
+                    <option value="from" >Van</option>
+                    <option value="to">Naar</option>
+              </select>
+              <br><br>
+              <p>Commentaar</p>
+              <textarea name="Comment" class="form-control" rows="5" maxlength="250" placeholder="Notities"></textarea>
+              <br><br>
+
+
+
+              <button type="submit" class="btn btn-default">Opslaan</button>
+            </div>
+          </form>
+    ';
